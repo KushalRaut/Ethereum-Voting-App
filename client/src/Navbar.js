f02bb483b9a8f5df7204680c7ebb4b10fb81b391
Kushal Raut <kushalraut5@gmail.com>
2021-11-10 19:42:25 +0545

    first commit

---
+++ client/src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = ({account}) =>{
+    return(
+        <div className="App">
+      <nav className="navbar navbar-dark bg-dark shadow mb-5">
+        <p className='navbar-brand my-auto m-3'>Electronic Voting First Draft</p>
+          <ul className="nav-item text-white m-3">Current Account:   {account}</ul>
+        </nav>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ contracts/Election.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+contract Election{
+    
+    // struct candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint votecount;
+    }
+    
+    // candidate count
+    uint public candidatesCount;
+    
+    // mapping candidate
+    mapping(uint => Candidate) public candidates;
+    
+    //mapping a voter
+    mapping(address=>bool) public votedornot;
+    
+    
+    // constructor
+    constructor(){
+        // Here the candidates are initialized
+        addCandidates("KP Oli");
+        addCandidates("Sher Bahadur Deuba");
+        addCandidates("Gagan Thapa");
+        addCandidates("Puspa Kamal Dahal");
+        addCandidates("Dr.Baburam Bhattarai");
+    
+    }
+    
+    //setting the addCandidates as private
+    function addCandidates(string memory name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount,name,0);
+    }
+    
+    //setting up the event that is to be triggered once the transaction is done i.e voting
+    event electionupdated(
+        uint id,
+        string name,
+        uint votecount
+    );
+    
+    
+    function Vote(uint _id) public{
+        //make sure that the candiate has not votes
+        require (!votedornot[msg.sender],'You have already voted');
+        
+        //make sure that the candidate is valid 
+        require(candidates[_id].id != 0,'The id does not exist');
+        
+        //now we increase the vote count of the candidate
+        candidates[_id].votecount += 1;
+        
+        //now setting the voted value of the voter to be true
+        votedornot[msg.sender] = true;
+        
+        emit electionupdated( _id, candidates[_id].name, candidates[_id].votecount);
+        
+        
+    }
+        
+}
