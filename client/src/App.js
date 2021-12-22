d9c57547727064438e6172a4636fe29944d97610
Nirajan995 <nirajankunwor604@gmail.com>
2021-12-22 10:17:12 +0545

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import OtpVerification from "./components/OtpVerification/OtpVerification";
 import { Route, Routes } from "react-router-dom";
 import VoterDashboard from "./screens/Voter/VoterDashboard";
 import ChatBot from "./screens/chatbot/ChatBot";
+import Vote from "./components/Vote/Vote";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/verify" element={<OtpVerification />} />
       <Route path="/voter/dashboard" element={<VoterDashboard />} />
       <Route path="/chatbot" element={<ChatBot />} />
+      <Route path="/vote" element={<Vote />} />
     </Routes>
   );
 };
