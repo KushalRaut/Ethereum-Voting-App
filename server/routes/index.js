8d89237e26d27eb21ad2c080b9fbc631fe66d727
Nirajan995 <nirajankunwor604@gmail.com>
2022-02-05 15:02:30 +0545

    merge resolved

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,6 @@ import userRoutes from "../routes/user.routes.js";
 const routes = Router();
 
 routes.use("/api/user", userRoutes);
-<<<<<<< HEAD
-routes.use("/api/candidate", candidateRoutes);
-=======
 // routes.use("/api/candidate/:id", candidateRoutes);
->>>>>>> ca5320d6043d2df83b22b9de41e971bb01698e6a
 
 export default routes;
