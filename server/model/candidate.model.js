68c4c71a1acd4e2a03b626a47092092caa457230
Nirajan995 <nirajankunwor604@gmail.com>
2022-02-05 15:08:58 +0545

    merge conflict resolved

--- server/model/candidate.model.js
+++ server/model/candidate.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
@@ -29,24 +28,3 @@ const candidateSchema = mongoose.Schema(
 );
 
 const Candidate = mongoose.model("candidates", candidateSchema);
-=======
-// import mongoose from "mongoose";
-
-// const candidateSchema = mongoose.Schema(
-//   {
-//     manifesto: {
-//       type: String,
-//     },
-//     user: {
-//       type: Schema.Types.ObjectId,
-//       ref: "users",
-//       required: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// const Candidate = mongoose.Schema("candidates", candidateSchema);
->>>>>>> ca5320d6043d2df83b22b9de41e971bb01698e6a
-
-// export default Candidate;
